081014bb81e19b2264678a8d5c133cb45a8f0a6a
Lucas Sannier <40730498+fox3000foxy@users.noreply.github.com>
2021-06-16 11:25:54 +0000

    Changes...

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Ca arrive
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"/public/index.html")
+});
+
+app.use(express.static("./public"))
+app.use(express.bodyParser())
+
+app.post('/',(req,res)=>{
+  console.log(req.body)
+  res.redirect("/")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
