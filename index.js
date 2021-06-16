522795b4d26e1f934ae6493d1977398cedb41eab
Lucas Sannier <40730498+fox3000foxy@users.noreply.github.com>
2021-06-16 15:08:43 +0000

    add some changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { Krypt, unKrypt } = require("./myKrypt")
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
@@ -12,6 +13,10 @@ app.post('/',(req,res)=>{
   console.log(req.body)
   res.redirect("/")
 })
+/*app.get('/test',(req,res)=>{
+  res.send(Krypt("mdp1234",3))
+})
+console.log(unKrypt("xqcmwgfdjytpfkp1bkh2ooy3rmc4§3"))*/
 
 app.use(express.static("./public"))
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
---
+++ myKrypt.js
@@ -0,0 +1,39 @@
+function genCharArray(charA, charZ) {
+    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
+    for (; i <= j; ++i) {
+        a.push(String.fromCharCode(i));
+    }
+    return a;
+}
+alphabet = genCharArray('a', 'z')
+
+function Krypt(myString,lvl=5){
+	if (lvl < 0) return "Unavailable level !"
+	actualHash = ""
+	for (i in myString){
+		for (j=0;j<lvl;j++){
+			randomNumber = Math.round(Math.random() * (alphabet.length-1))
+			randomLetter = alphabet[randomNumber]
+			actualHash += randomLetter
+		}
+		actualHash += myString[i]
+	}
+	actualHash += "§"+lvl
+	return actualHash
+}
+
+function unKrypt(hash){
+	key = parseInt(hash.split("§")[1])
+	actualString = ""
+	i=key
+	while (i<hash.length){
+		//if (hash.charAt(i)!=key.toString() && key!=1)
+		actualString += hash.charAt(i)
+		i += key+1
+	}
+	return actualString
+}
+
+module.exports = {unKrypt,Krypt}
+
+//console.log(unKrypt("mmcioKtblusaqcnkdrgczdmistzhumkvujb xvcgcctdtoi'hynonetdiicsgnytktocpef ohgmolyejlpepxxot qxioebfkqeug§5"))
