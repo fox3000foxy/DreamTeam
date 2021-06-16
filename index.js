f66537708bd6160e77bdfb4b443c2b759798a02b
Lucas Sannier <40730498+fox3000foxy@users.noreply.github.com>
2021-06-16 11:37:44 +0000

    Fix body parser

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
 app.get("/",(req,res)=>{
   res.sendFile(__dirname+"/public/index.html")
 });
 
-app.use(express.static("./public"))
-app.use(express.bodyParser())
-
 app.post('/',(req,res)=>{
   console.log(req.body)
   res.redirect("/")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.use(express.static("./public"))
+app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
